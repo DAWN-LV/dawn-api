8c0f02c637c2754f041c63d544ff83071cd7e59e
Vitalijs Pankovs <vitalijs.pankovs@gmail.com>
2024-05-19 17:36:53 +0300

    Added modules exports.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,2 @@
-import { generateSpecification, specification } from "./specification"
-import { IOptions } from "./types"
-import { Express } from "express"
-import { metadata } from "./globals"
-import path from "path"
-
-abstract class RapidocModule {
-  static async createDocument(app: Express, options: IOptions) {
-    options.openapi = options.openapi || { enabled: true, publicURL: "http://[publicURl]" } 
-
-    generateSpecification(metadata, options)
-
-    app.get("/specification", (_, res) => res.send(specification))
-    app.get(options.basePath, (_, res) => {
-      res.sendFile(path.join(__dirname, "..", "public", "index.html"))
-    })
-  }
-}
-
-export { RapidocModule }
+export * from "./decorators"
+export * from "./modules"
---
+++ src/modules/rapidoc/index.ts
@@ -0,0 +1,20 @@
+import path from "path"
+import { Express } from "express"
+import { IOptions } from "../../types"
+import { generateSpecification, specification } from "../../specification"
+import { metadata } from "../../globals"
+
+abstract class RapidocModule {
+  static async createDocument(app: Express, options: IOptions) {
+    options.openapi = options.openapi || { enabled: true, publicURL: "http://[publicURl]" } 
+
+    generateSpecification(metadata, options)
+
+    app.get("/specification", (_, res) => res.send(specification))
+    app.get(options.basePath, (_, res) => {
+      res.sendFile(path.join(__dirname, "..", "public", "index.html"))
+    })
+  }
+}
+
+export { RapidocModule }
